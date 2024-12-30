1d811df47ddd629223f9eadb5b089221efe37775
Meghana Ganesh <meghanavlsi08@gmail.com>
2024-12-30 12:56:20 +0530

    Create testbench.v

---
+++ day-94/testbench.v
@@ -0,0 +1,32 @@
+
+module tb_110_mealy_seq_detector();
+wire  seq_detected;
+reg rst,clk,seq_in;
+
+mealy_seq_detector_110  dut ( .clk(clk),
+                               .rst(rst),
+                               .seq_in(seq_in),
+                               .seq_detected(seq_detected) );
+     
+always #5 clk=~clk;
+initial begin 
+clk=0;rst=1;seq_in=0;
+#10
+rst=0; 
+
+#10 seq_in=1;
+#10 seq_in=1;
+#10 seq_in=0;
+
+#10 seq_in=1;
+#10 seq_in=1;
+#10 seq_in=0;
+
+#10 seq_in=1;
+#10 seq_in=1;
+#10 seq_in=0;
+
+#40 $stop();
+
+end
+endmodule
